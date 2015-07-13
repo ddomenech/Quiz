3e5c51e9aa827fde94215c60dccb6d36c2cbe6bc
David Domenech <ddomenech@gmail.com>
2015-07-14 01:39:38 +0200

    incluir express-partials y cambiar plantillas

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,13 @@
+//Get /quizes/question
+exports.question = function(req, res, next) {
+  res.render('quizes/question', {pregunta: 'Capital de Italia'});  
+};
+
+//GET /quizes/answer
+exports.answer = function(req, res, next) {
+  if (req.query.respuesta === 'Roma') {
+   res.render('quizes/answer', {respuesta: 'Correcto'});     
+  }else{
+   res.render('quizes/answer', {respuesta: 'Incorrecto'});
+  }
+};

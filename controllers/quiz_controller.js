0bc020dea7835e84db7050b5dcb242f403112fbf
David Domenech <ddomenech@gmail.com>
2015-07-14 22:43:51 +0200

    Cambios realizados por el Ejercicio Obligatorio del Modulo 6

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,6 +1,6 @@
 //Get /quizes/question
 exports.question = function(req, res, next) {
-  res.render('quizes/question', {pregunta: 'Capital de Italia'});  
+  res.render('quizes/question', {pregunta: '¿Capital de Italia?'});  
 };
 
 //GET /quizes/answer
@@ -10,4 +10,9 @@ exports.answer = function(req, res, next) {
   }else{
    res.render('quizes/answer', {respuesta: 'Incorrecto'});
   }
+};
+
+//GET /author
+exports.author = function(rq, res, next) {
+  res.render('creditos/author', {author: 'David Domenech', foto: 'images/foto.png' });  
 };

b07eeea3d00c28a96090dab71fe49514b0b1c5c8
David Domenech <ddomenech@gmail.com>
2015-07-24 02:09:30 +0200

    base de datos heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,18 +1,25 @@
+
 //Get /quizes/question
+var models = require('../models/models.js');
+
 exports.question = function(req, res, next) {
-  res.render('quizes/question', {pregunta: '¿Capital de Italia?'});  
+  models.Quiz.findAll().then(function(quiz){
+    res.render('quizes/question', {pregunta: quiz[0].pregunta});
+  });
 };
 
 //GET /quizes/answer
 exports.answer = function(req, res, next) {
-  if (req.query.respuesta === 'Roma') {
-   res.render('quizes/answer', {respuesta: 'Correcto'});     
-  }else{
-   res.render('quizes/answer', {respuesta: 'Incorrecto'});
-  }
+  models.Quiz.findAll().then(function(quiz) {
+    if (req.query.respuesta === quiz[0].respuesta) {
+     res.render('quizes/answer', {respuesta: 'Correcto'});
+    } else {
+     res.render('quizes/answer', {respuesta: 'Incorrecto'});
+    }
+  });
 };
 
 //GET /author
 exports.author = function(rq, res, next) {
-  res.render('creditos/author', {author: 'David Domenech', foto: 'images/foto.png' });  
+  res.render('creditos/author', {author: 'David Domenech', foto: 'images/foto.png' });
 };
